537138b225be2d54435fb264e193512cc42e73e4
Santi Espinosa <saigesp@gmail.com>
2024-09-24 19:15:02 +0200

    change zoom

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 
-const map = L.map('map').setView([39.8376017, -4.3978819], 6);
+const map = L.map('map').setView([39.8376017, -4.3978819], 5);
 L.tileLayer.provider('CartoDB.Voyager').addTo(map);
 
 const markers = [];
